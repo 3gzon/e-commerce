caf2c844bbc79d600d90017a722e46319905100e
Egzon <egzonuka8@gmail.com>
2022-06-20 15:54:10 +0200

    feat: adding style to navigation header

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,15 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
-
+import { ReactComponent as EcommerceLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          <EcommerceLogo className="logo" />
         </Link>
-        <div className="links-container">
+        <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
